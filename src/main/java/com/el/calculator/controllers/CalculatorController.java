f635e906eacbce057ce28ad216ecfb2ea1a04f90
Tom Carrio <Thomas.Carrio@Ally.com>
2018-06-15 14:09:46 -0400

    Added skeleton methods

--- src/main/java/com/el/calculator/controllers/CalculatorController.java
+++ src/main/java/com/el/calculator/controllers/CalculatorController.java
@@ -2,4 +2,20 @@ package com.el.calculator.controllers;
 
 public class CalculatorController {
 
+	
+	private int add(int first, int second) {
+		
+	}
+	
+	private int subtract(int first, int second) {
+		
+	}
+	
+	private int multiply(int first, int second) {
+		
+	}
+	
+	private int divide(int first, int second) {
+		
+	}
 }
